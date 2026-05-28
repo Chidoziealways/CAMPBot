839b6c6d5c9733f6613171c428df7fd400f5cc65
Chidoziealways <uzowuluchidozie@gmail.com>
2026-05-28 18:41:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+﻿import discord
+import os
+token = os.getenv("CAMPBOT_TOKEN")
+if token is None:
+    raise RuntimeError("Missing CAMPBOT_TOKEN")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print(f"Logged ")
+
+@tree.command(name="ping")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+
+@tree.command(name="kill")
+async def kill(interaction: discord.Interaction, tokill: discord.Member):
+    await interaction.response.send_message(f"Killing {tokill.mention}")
+
+client.run(token)
+
+#lil int 6193533967284288

9ac6721bb5a0611d738cbac9632b94646f4da0e7
Chidoziealways <uzowuluchidozie@gmail.com>
2026-05-29 16:34:00 +0100

    Added ModCommands and a Marry command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 ﻿import discord
+from discord import app_commands
+import marry
 import os
 token = os.getenv("CAMPBOT_TOKEN")
 if token is None:
@@ -8,7 +10,22 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
+#mod group
+mod_group = discord.app_commands.Group(
+    name="mod",
+    description="Moderation Commands"
+)
+
+#marry group
+marry_group = discord.app_commands.Group(
+    name="marry",
+    description="Marriage related commands"
+)
+
 synced = False
+
+tree.add_command(mod_group)
+tree.add_command(marry_group)
 
 @client.event
 async def on_ready():
@@ -26,10 +43,54 @@ async def ping(interaction: discord.Interaction):
 async def kill(interaction: discord.Interaction, tokill: discord.Member):
     await interaction.response.send_message(f"Killing {tokill.mention}")
 
-@tree.command(name="marry")
+@marry_group.command(name="marry")
 async def marry(interaction: discord.Interaction, tomarry: discord.Member):
     sent_user = interaction.user
-    await interaction.response.send_message(f"{sent_user.mention} has is married to {tomarry.mention}!")
+    view = marry.MarryView(sent_user, tomarry)
+    await interaction.response.send_message(f"{sent_user.mention} has proposed to {tomarry.mention}!",
+                                            view=view)
+
+@mod_group.command(name="ban")
+@app_commands.default_permissions(
+    ban_members=True
+)
+async def ban(interaction: discord.Interaction, toban: discord.Member, reason: str):
+    if not interaction.user.guild_permissions.ban_members:
+        await interaction.response.send_message(
+            "No permission.",
+            ephemeral=True
+        )
+        return
+    await interaction.response.send_message(f"Banning {toban.mention}")
+    await toban.ban(reason=reason)
+
+@mod_group.command(name="kick")
+@app_commands.default_permissions(
+    kick_members=True
+)
+async def kick(interaction: discord.Interaction, tokick: discord.Member, reason: str):
+    if not interaction.user.guild_permissions.kick_members:
+        await interaction.response.send_message(
+            "No permission.",
+            ephemeral=True
+        )
+        return
+    await interaction.response.send_message(f"Kicking {tokick.mention}")
+    await tokick.kick(reason=reason)
+
+@mod_group.command(name="unban")
+@app_commands.default_permissions(
+    ban_members=True
+)
+async def unban(interaction: discord.Interaction, tounban: discord.Member, reason: str):
+    if not interaction.user.guild_permissions.ban_members:
+        await interaction.response.send_message(
+            "No permission.",
+            ephemeral=True
+        )
+        return
+    await interaction.response.send_message(f"Unbanning {tounban.mention}")
+    await tounban.unban(reason=reason)
 
 
 client.run(token)
---
+++ marry.py
@@ -0,0 +1,54 @@
+﻿import discord
+from discord import app_commands
+
+class MarryView(discord.ui.View):
+    proposer: discord.Member
+    proposed: discord.Member
+
+    def __init__(self, proposer, proposed):
+        super().__init__(timeout=120)
+        self.proposer = proposer
+        self.proposed = proposed
+
+    @discord.ui.button(
+        label="Accept",
+        style=discord.ButtonStyle.green
+    )
+    async def accept(
+            self: MarryView,
+            interaction: discord.Interaction,
+            button: discord.ui.Button,
+    ):
+        if interaction.user != self.proposed:
+            await interaction.response.send_message(
+                "Not your proposal.",
+                ephemeral=True
+            )
+            return
+
+        await interaction.response.send_message(
+            f"{self.proposed.mention} accept the proposal!"
+        )
+        await interaction.response.send_message(
+            f"{self.proposer.mention} and {self.proposed.mention} are now married!"
+        )
+
+    @discord.ui.button(
+        label="Decline",
+        style=discord.ButtonStyle.red
+    )
+    async def decline(
+            self: MarryView,
+            interaction: discord.Interaction,
+            button: discord.ui.Button
+    ):
+        if interaction.user != self.proposed:
+            await interaction.response.send_message(
+                "Not your proposal!",
+                ephemeral=True
+            )
+            return
+
+        await interaction.response.send_message(
+            f"{self.proposed.mention} declined!"
+        )
